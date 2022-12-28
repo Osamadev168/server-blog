c7589804bcfc284cd90c230bdf76c00a65e281b5
Osamadev168 <osamatwenty@gmail.com>
2022-12-28 12:51:37 +0500

    implemented JWT

--- Controller/user-controller.js
+++ Controller/user-controller.js
@@ -1,5 +1,7 @@
 import bycrpyt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../Model/user.js";
+import token from "../Model/token.js";
 export const signup = async (req, res) => {
   try {
     const hashedPassword = await bycrpyt.hash(req.body.password, 6);
@@ -19,5 +21,37 @@ export const signup = async (req, res) => {
     }
   } catch (e) {
     return res.status(500).json({ msg: "Error while adding user!" });
+  }
+};
+export const signin = async (req, res) => {
+  let user = await User.findOne({ username: req.body.username });
+  if (!user) {
+    return res
+      .status(404)
+      .json({ msg: "User not found or Account doesnt exists" });
+  }
+  try {
+    let matchPassword = await bycrpyt.compare(req.body.password, user.password);
+    if (matchPassword) {
+      const accesstoken = jwt.sign(user.toJSON(), process.env.AccessToken, {
+        expiresIn: "10m",
+      });
+      const refreshToken = jwt.sign(user.toJSON(), process.env.RefreshToken);
+
+      const newToken = new token({ token: refreshToken });
+      await newToken.save();
+      return res
+        .status(200)
+        .json({
+          name: user.name,
+          username: user.username,
+          accesstoken: accesstoken,
+          refreshToken: refreshToken,
+        });
+    } else {
+      res.status(404).json({ msg: "Incorrect pssword" });
+    }
+  } catch (e) {
+    res.status(500).json({ msg: "Failure" });
   }
 };
--- Routes/router.js
+++ Routes/router.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { signup } from "../Controller/user-controller.js";
+import { signin, signup } from "../Controller/user-controller.js";
 const Router = express.Router();
 Router.post("/signup", signup);
+Router.post("/signin", signin);
+
 export default Router;
