6ececeee59f73bcf532617a7a0c31b557b8bd78e
Osamadev168 <osamatwenty@gmail.com>
2023-03-19 00:15:18 +0500

    big

--- index.js
+++ index.js
@@ -9,20 +9,10 @@ dotenv.config();
 const App = express();
 App.use(
   cors({
-    origin: ["https://www.hubblefeed.com/", "http://localhost:7000"],
+    origin: ["https://www.hubblefeed.com", "http://localhost:7000"],
   })
 );
-App.all("*", function (req, res, next) {
-  const origin = cors.origin.includes(req.header("origin").toLowerCase())
-    ? req.headers.origin
-    : cors.default;
-  res.header("Access-Control-Allow-Origin", origin);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 App.use(helmet());
 App.use(express.json({ limit: "50mb" }));
 App.use(bodyParser.json({ limit: "50mb", extended: true }));
