e06282705abf87a0c4c3081601bd0c51cc71c02c
Osamadev168 <osamatwenty@gmail.com>
2023-03-27 22:51:41 +0500

    cors

--- index.js
+++ index.js
@@ -7,17 +7,11 @@ import Router from "./Routes/router.js";
 import { Connection } from "./Database/db.js";
 dotenv.config();
 const App = express();
-var allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", [
-    "https://www.hubblefeed.com",
-    "http://localhost:7000",
-  ]);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-};
-App.use(allowCrossDomain);
-App.use(cors());
+App.use(
+  cors({
+    origin: ["https://www.hubblefeed.com/", "http://localhost:5173"],
+  })
+);
 App.use(helmet());
 App.use(express.json({ limit: "50mb" }));
 App.use(bodyParser.json({ limit: "50mb", extended: true }));
