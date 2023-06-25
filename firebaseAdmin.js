971f50f87318ea826f3aed86d50ffbc79656801b
Osama Habib <84494988+Osamadev168@users.noreply.github.com>
2023-06-25 21:54:50 +0500

    commit

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,12 +1,9 @@
 import Admin from "firebase-admin";
-// import service_account from "./ServiceAccount.json" assert { type: "json" };
 import dotenv from "dotenv";
-const service_account = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON;
 dotenv.config();
 Admin.initializeApp({
   credential: Admin.credential.cert(
     JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON)
   ),
 });
-
 export default Admin;
